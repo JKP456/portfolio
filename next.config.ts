a0252d1bd4aaacefaea5ce6bc2a7fb6da285caf0
Bento12ad <nongkhun2015@gmail.com>
2026-02-17 01:33:43 +0700

    setup: github pages export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // สำคัญมาก
+  images: {
+    unoptimized: true, // GitHub Pages ไม่รองรับ Image Optimization
+  },
+  // ถ้าโปรเจกต์คุณไม่ได้อยู่ที่ root (เช่น github.com/user/repo-name) 
+  // ให้ใส่ basepath: '/repo-name' ด้วย แต่ถ้าเป็น domain หลักไม่ต้องครับ
 };
-
 export default nextConfig;
